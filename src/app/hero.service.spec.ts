2a65e1e87306e9923cdf0703a3160c7d9558c802
tc <theairportexplorer@gmail.com>
2020-04-09 12:49:31 -0400

    finished deep integration test moduel

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,38 @@
+import { TestBed } from "@angular/core/testing";
+import { HeroService } from "./hero.service";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { MessageService } from "./message.service";
+import { inject } from "@angular/core";
+
+describe('HeroService', () => {
+  let mockMessageService
+  let httpTestingController: HttpTestingController;
+  let service: HeroService;
+
+  beforeEach(() => {
+    mockMessageService = jasmine.createSpyObj(['add']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        HeroService,
+        { provide: MessageService, useValue: mockMessageService }
+      ]
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(HeroService);
+  });
+
+  describe('getHero', () => {
+    it('should call get with the correct URL', () => {
+      service.getHero(4).subscribe(() => {
+        console.log('fulfilled');
+      });
+
+      const req = httpTestingController.expectOne('api/heroes/4');
+      req.flush({id: 4, name: 'SuperDude', strength: 100});
+      httpTestingController.verify();
+    });
+  });
+});
